88448733d12ae5947d45323f64310aa55150fee2
plaffey <peter.laffey@gmail.com>
2016-07-20 22:30:15 -0500

    Updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,46 @@ var path = require("path");
 
 module.exports = {
   entry: [
-    './src/index.js'
+    "script!jquery/dist/jquery.min.js",
+    "script!foundation-sites/dist/foundation.min.js",
+    "./app/app.jsx"
+  ],
+  externals: {
+    jquery: "jQuery"
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      "$": "jquery",
+      "jQuery": "jquery"
+    })
   ],
   output: {
     path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
+    filename: "./public/bundle.js"
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    root: __dirname,
+    alias: {
+      applicationStyles: "app/styles/style.css"
+    },
+    extensions: ["", ".js", ".jsx"]
   },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
+  module: {
+    loaders: [
+      {
+        loader: "babel-loader",
+        query: {
+          presets: ["react", "es2015", "stage-0"]
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  sassLoader: {
+    includePaths: [
+      path.resolve(__dirname, "./node_modules/foundation-sites/scss")
+    ]
+  },
+  devtool: "cheap-module-eval-source-map"
 };
